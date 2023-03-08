f3ccc8d5c17451f7f088775f0170b82e6d936e56
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-03-08 22:04:08 +0200

    1004

--- src/components/ranks/RankDashboard.jsx
+++ src/components/ranks/RankDashboard.jsx
@@ -4,13 +4,18 @@ import { readUser } from '../../store/actions/users/usersActions';
 import { StatsList } from '../stats';
 
 export const RankDashboard = ({ userId }) => {
+  // busy, setBusy useState
   const dispatch = useDispatch();
   const user = useSelector(({ users }) => {
     return users.entities[userId];
   });
 
   useEffect(() => {
+    // setBusy
+
+    // await
     dispatch(readUser(userId));
+    // setBusy
   }, [dispatch, userId]);
 
   if (!user) {
